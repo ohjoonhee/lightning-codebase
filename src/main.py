387003a5455fba98fe85fb8b35b7c10037cdcb09
ohjoonhee <ohjoonhee@snu.ac.kr>
2024-07-01 22:04:18 +0900

    feat: add args, run arguments

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import torch
 
 import lightning as L
+from lightning.pytorch.cli import ArgsType
+from argparse import ArgumentError
 
 try:
     if os.environ.get("WANDB_DISABLED", False):
@@ -20,16 +22,45 @@ import policy
 import model
 
 
-def cli_main():
+def cli_main(args: ArgsType = None, run: bool = True):
     torch.set_float32_matmul_precision("medium")
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
-    cli = CLI(
-        L.LightningModule,
-        parser_kwargs={
-            "parser_mode": "omegaconf",
-        },
-        subclass_mode_model=True,
-    )
+    try:
+        cli = CLI(
+            L.LightningModule,
+            L.LightningDataModule,
+            parser_kwargs={
+                "parser_mode": "omegaconf",
+                "exit_on_error": False,
+            },
+            subclass_mode_model=True,
+            subclass_mode_data=True,
+            args=args,
+            run=run,
+        )
+    except ArgumentError as e:
+        try:
+            logger.warning(
+                "DataModule is not implemented! Using only LightningModule..."
+            )
+            cli = CLI(
+                L.LightningModule,
+                parser_kwargs={
+                    "parser_mode": "omegaconf",
+                    "exit_on_error": False,
+                },
+                subclass_mode_model=True,
+                args=args,
+                run=run,
+            )
+        except Exception as e:
+            raise e from None
+    except Exception as e:
+        raise e
+
+    return cli
 
 
 if __name__ == "__main__":
