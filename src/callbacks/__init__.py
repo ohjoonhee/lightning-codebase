1234f1c9ef2319f31c7b3fb5b13b88fdf0ddc3f7
ohjoonhee <ohjoonhee@snu.ac.kr>
2024-07-12 03:19:10 +0900

    feat: wandb alert callback

---
+++ src/callbacks/__init__.py
@@ -0,0 +1,1 @@
+from . import wandb_alert
---
+++ src/callbacks/wandb_alert.py
@@ -0,0 +1,32 @@
+from typing import Literal
+
+import torch
+import lightning as L
+from lightning.pytorch.loggers import WandbLogger
+
+try:
+    import wandb
+
+    _WANDB_AVAILABLE = True
+except ImportError:
+    _WANDB_AVAILABLE = False
+
+
+class WandbAlert(L.Callback):
+    def __init__(self, monitor: str, mode: Literal["min", "max"]) -> None:
+        super().__init__()
+        self.monitor = monitor
+        self.mode = mode
+        self.best_metric = float("inf") if mode == "min" else float("-inf")
+        self.monitor_op = torch.lt if mode == "min" else torch.gt
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        if not isinstance(trainer.logger, WandbLogger):
+            return
+        if self.monitor_op(trainer.callback_metrics[self.monitor], self.best_metric):
+            self.best_metric = trainer.callback_metrics[self.monitor]
+            trainer.logger.experiment.alert(
+                title="Metric improved",
+                text=f"{self.monitor}={self.best_metric:.4f}\nval_loss={trainer.callback_metrics['val_loss']:.6f}\nepoch={trainer.current_epoch}",
+                wait_duration=1,
+            )
